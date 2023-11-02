c228c9cda9ac38a5c9a6257609b98d14e4f12de4
Ivan G Stewart <71293769+ivanstewart2001@users.noreply.github.com>
2023-11-02 08:54:08 -0400

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+
+@Module({
+  imports: [
+    ProductsModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://geoffreystewart84:Familyof7@cluster0.0gns5zo.mongodb.net/nestjs-demo?retryWrites=true&w=majority',
+    ),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
